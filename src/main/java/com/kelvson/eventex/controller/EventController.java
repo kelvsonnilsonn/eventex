66b1203dbe3d6d0652c6508cad8c0366147bd90c
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-11-10 18:43:57 -0300

    feat(controller): add @Valid annotation to make validation annotations in command works correctly

--- src/main/java/com/kelvson/eventex/controller/EventController.java
+++ src/main/java/com/kelvson/eventex/controller/EventController.java
@@ -7,6 +7,7 @@ import com.kelvson.eventex.command.UpdateEventNameCommand;
 import com.kelvson.eventex.service.command.EventCommandService;
 import com.kelvson.eventex.service.query.EventQueryService;
 import com.kelvson.eventex.util.AppConstants;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
@@ -22,24 +23,24 @@ public class EventController {
     private final EventQueryService eventQueryService;
 
     @PostMapping
-    public ResponseEntity<Long> create(@RequestBody CreateEventCommand command){
+    public ResponseEntity<Long> create(@Valid @RequestBody CreateEventCommand command){
         return ResponseEntity.status(HttpStatus.CREATED).body(eventCommandService.create(command));
     }
 
     @DeleteMapping
-    public ResponseEntity<Void> delete(@RequestBody DeleteEventCommand command){
+    public ResponseEntity<Void> delete(@Valid @RequestBody DeleteEventCommand command){
         eventCommandService.delete(command);
         return ResponseEntity.status(HttpStatus.OK).build();
     }
 
     @PutMapping(AppConstants.CHANGE_EVENT_NAME)
-    public ResponseEntity<Void> updateName(@RequestBody UpdateEventNameCommand command) {
+    public ResponseEntity<Void> updateName(@Valid @RequestBody UpdateEventNameCommand command) {
         eventCommandService.update(command);
         return ResponseEntity.ok().build();
     }
 
     @PutMapping(AppConstants.CHANGE_DESCRIPTION_NAME)
-    public ResponseEntity<Void> updateDescription(@RequestBody UpdateEventDescriptionCommand command) {
+    public ResponseEntity<Void> updateDescription(@Valid @RequestBody UpdateEventDescriptionCommand command) {
         eventCommandService.update(command);
         return ResponseEntity.ok().build();
     }
