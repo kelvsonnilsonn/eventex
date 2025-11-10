6c4b041a14658e6e5882d4fce20e8a2b101d0515
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-11-10 18:42:33 -0300

    feat(util): add change paths in AppConstants
    - add VALIDATION MESSAGES in AppConstants : not null

--- src/main/java/com/kelvson/eventex/util/AppConstants.java
+++ src/main/java/com/kelvson/eventex/util/AppConstants.java
@@ -7,4 +7,6 @@ public class AppConstants {
     public static final String EVENT_PATH = "/api/v1/event";
     public static final String CHANGE_EVENT_NAME = "/change-name";
     public static final String CHANGE_DESCRIPTION_NAME = "/change-description";
+
+    public static final String NOT_NULL_MESSAGE = " não pode ser nulo.";
 }
