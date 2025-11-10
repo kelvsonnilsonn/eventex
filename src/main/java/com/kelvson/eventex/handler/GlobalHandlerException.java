1cc80ba8a9fd559515b5f8686c204ab6773985d7
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-11-10 18:45:46 -0300

    feat(handler): add MethodArgumentNotValidException handler in GlobalHandlerException to make spring response validation annotation messages as API error response

--- src/main/java/com/kelvson/eventex/handler/GlobalHandlerException.java
+++ src/main/java/com/kelvson/eventex/handler/GlobalHandlerException.java
@@ -3,8 +3,12 @@ package com.kelvson.eventex.handler;
 import com.kelvson.eventex.exception.EventNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class GlobalHandlerException {
@@ -12,5 +16,16 @@ public class GlobalHandlerException {
     @ExceptionHandler(EventNotFoundException.class)
     public ResponseEntity<String> eventNotFoundHandler(EventNotFoundException e){
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        String errorMessage = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(FieldError::getDefaultMessage)
+                .collect(Collectors.joining("; "));
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorMessage);
     }
 }
