8dc959c5b4dddf783ce434cfca4a6b47c7c2653d
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-11-10 21:00:35 -0300

    refactor(repository): add @Query annotation to get all similar event names in the search request

--- src/main/java/com/kelvson/eventex/repository/EventRepository.java
+++ src/main/java/com/kelvson/eventex/repository/EventRepository.java
@@ -1,11 +1,13 @@
 package com.kelvson.eventex.repository;
 
 import com.kelvson.eventex.model.Event;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
+import org.springframework.data.jpa.repository.Query;
 
 public interface EventRepository extends JpaRepository<Event, Long> {
 
-    Optional<Event> findByEventName(String eventName);
+    @Query("SELECT e FROM Event e WHERE LOWER(e.eventName) LIKE LOWER(CONCAT('%', :eventName, '%'))")
+    Page<Event> findByEventName(Pageable pageable, String eventName);
 }
