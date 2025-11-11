e5bfc079279ac029ac03c389c616d4620d68b7e3
Kelvson Nilson <kelvsonnilson01@gmail.com>
2025-11-10 21:02:12 -0300

    fix(mapper): add @Mapping annotation in toResponse method because the method wasn't getting event id and giving it to EventResponseDTO

--- src/main/java/com/kelvson/eventex/mapper/EventMapper.java
+++ src/main/java/com/kelvson/eventex/mapper/EventMapper.java
@@ -4,12 +4,14 @@ import com.kelvson.eventex.command.CreateEventCommand;
 import com.kelvson.eventex.dto.EventResponseDTO;
 import com.kelvson.eventex.model.Event;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface EventMapper {
 
     Event toEntity(CreateEventCommand dto);
 
+    @Mapping(target = "eventId", source = "id")
     EventResponseDTO toResponse(Event event);
 
 }
